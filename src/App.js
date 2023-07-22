3cea3943f9167c537b35ffddb83c6c4cdff2ca14
Madcodes831 <r.medina2119@gmail.com>
2023-07-21 18:50:59 -0700

    All done with sprint 6

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-
+import Character from './components/Character';
 const URL = 'https://swapi.dev/api/people/'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [ data, setData ] = useState()
+  const [ characters, setCharacters ] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(()=>{
-    function fetchCharacters() {
         axios.get(URL)
         .then(res => {
-          setData(res.data)
+          setCharacters(res.data)
+          
         })
         .catch(err=>{
           console.log(err.message)
         })
-
-    }
-   fetchCharacters()
   }, [])
-  if( !data ) return 'Fetching list of Characters...'
+ 
   return (
   <section>
      <div className="App">
       <h1 className="Header"> Galaxy Warriors </h1>
-
+      <Character data= {characters}/>
       
     </div>
   </section>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,80 @@
-// Write your Character component here
+import React, {useState} from 'react'
+import styled from 'styled-components';
+
+const Card = styled.div`
+
+background-color: black;
+width: 300px;
+border: 15px solid grey;
+padding: 50px;
+margin: auto;
+
+`
+
+const CharacterList = styled.div`
+
+font-family:'Gill Sans'
+
+
+`
+const CharName= styled.h2`
+font-family: 'bungee spice'
+
+`
+const Details= styled.li`
+display:flex;
+justify-content:space-around;
+color: white;
+font-size: 20px;
+font-family: cursive;
+
+`
+const CollapseButton = styled.button`
+margin-top: 5px;
+`
+  
+;
+
+
+const Character= (props) => {
+    const { data } = props;
+    const [detailsVisible, setDetailsVisible] = useState({});
+
+    // Function to toggle the visibility of character details
+    const toggleDetails = (characterName) => {
+      setDetailsVisible((prevDetailsVisible) => ({
+        ...prevDetailsVisible,
+        [characterName]: !prevDetailsVisible[characterName],
+      }));
+    };
+    return (
+        <Card>
+            {data.map((character) => {
+                const isDetailsVisible = detailsVisible[character.name] || false;
+             return(
+
+                 <CharacterList>
+                   <CharName><br></br>{character.name}</CharName>
+                   <CollapseButton onClick={() => toggleDetails(character.name)}>
+                    {isDetailsVisible ? 'Collapse' : 'Expand'}
+                       </CollapseButton>
+                {isDetailsVisible && ( 
+                    <>  
+                        <Details> DOB: {character.birth_year}</Details>
+                        <Details> Gender: {character.gender}</Details>
+                        <Details> Height: {character.height}</Details>
+                    </>
+                    )}  
+                 </CharacterList>
+             )
+             
+       })}
+
+    </Card>
+
+)
+
+
+
+}
+export default Character;
